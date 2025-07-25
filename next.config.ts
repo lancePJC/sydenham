cc410c20cf7f4d00905733ab2e75f8a98cc436c0
lancePJC <peter24meena@gmail.com>
2025-07-25 14:48:53 +0300

    updates3

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig = {
   },
   output: 'export',
   images: { unoptimized: true },
-  basePath: '/your-repo-name', // Replace with your repo name
+  basePath: '/sydenham', // Replace with your repo name
 };
 
 export default nextConfig;
